e91ed095762a1384972993049cea97742092a4d6
AmandaSil <itzel.silverio3684@alumnos.udg.mx>
2023-10-11 18:22:03 -0600

    Ejercicio Graficas dinamicas terminado

---
+++ Dinamic_graphs.R
@@ -0,0 +1,59 @@
+library(plotly)
+library(ggplot2)
+library(tidyverse)
+
+fert<-read.csv("https://raw.githubusercontent.com/kitadasmalley/FA2020_DataViz/main/data/gapminderFert.csv", 
+               header=TRUE)
+
+head(fert)
+str(fert)
+summary(fert)
+
+
+#Fertility rate---------------------
+
+library(dplyr)
+library(ggplot2)
+p<- fert%>%
+  filter(year == 2015)%>%
+  ggplot(aes(fert, life, size = pop, color = continent)) +
+  labs(x="Fertility Rate", y = "Life expectancy at birth (years)", 
+       caption = "(Based on data from Hans Rosling - gapminder.com)", 
+       color = 'Continent',size = "Population (millions)") + 
+  ylim(30,100) +
+  geom_point()
+  ggplotly(p)
+
+#Dynamic graph---------------------
+#install.packages("plotly")
+library(plotly)
+library(ggplot2)
+p<- fert%>%
+  ggplot(aes(x=fert, y=life, size = pop, color = continent,frame = year)) +
+  labs(x="Fertility Rate", y = "Life expectancy at birth (years)", 
+       caption = "(Based on data from Hans Rosling - gapminder.com)", 
+       color = 'Continent',size = "Population (millions)") + 
+  ylim(30,100) +
+  geom_point(aes(text=Country))
+
+ggplotly(p)
+
+
+#Using the gganimate-------------------------
+library(gganimate)
+library(gifski)
+
+p1 <- ggplot(fert, aes(fert, life, size = pop, color = continent, frame = year)) +
+  labs(x="Fertility Rate", y = "Life expectancy at birth (years)", 
+       caption = "(Based on data from Hans Rosling - gapminder.com)", 
+       color = 'Continent',size = "Population (millions)") + 
+  ylim(30,100) +
+  geom_point() +
+  #ggtitle("Year: {frame_time}") +
+  transition_time(year) +
+  ease_aes("linear") +
+  enter_fade() +
+  exit_fade()
+
+animate(p1,fps = 4, width = 700, height = 500, renderer = gifski_renderer())
+
